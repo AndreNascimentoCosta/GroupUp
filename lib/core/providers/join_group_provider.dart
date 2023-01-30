52284cea0ed1551d9a13154f8671c7b7410b62ca
Andre Costa <andrencosta@outlook.com>
2023-01-30 15:26:23 -0300

    feat: return errr when join group doesnt work

--- lib/core/providers/join_group_provider.dart
+++ lib/core/providers/join_group_provider.dart
@@ -1,9 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:groupup/models/group_model.dart';
 import 'package:groupup/models/participant.dart';
 import 'package:groupup/core/providers/auth_provider.dart';
 import 'package:provider/provider.dart';
+
+enum JoinGroupErrorType {
+  groupCodeEmpty,
+  groupCodeInvalid,
+  groupCodeExpired,
+  groupCodeFull,
+  userAlreadyInGroup,
+}
 
 class JoinGroupProvider extends ChangeNotifier {
   final controllerGroupCode = TextEditingController();
@@ -22,17 +31,57 @@ class JoinGroupProvider extends ChangeNotifier {
     if ((joinGroupText.isEmpty)) {
       return null;
     } else {
-      return () => {
-            FocusNode().unfocus(),
-            joinGroup(context),
-            clean(),
-            Navigator.pop(context),
-            Navigator.pop(context),
-          };
+      return () async {
+        final scaffoldMessengerState = ScaffoldMessenger.of(context);
+        final navigatorState = Navigator.of(context);
+        FocusNode().unfocus();
+        final error = await joinGroup(context);
+        switch (error) {
+          case JoinGroupErrorType.groupCodeEmpty:
+            scaffoldMessengerState.showSnackBar(
+              const SnackBar(
+                content: Text('Please enter a group code'),
+              ),
+            );
+            break;
+          case JoinGroupErrorType.groupCodeInvalid:
+            scaffoldMessengerState.showSnackBar(
+              const SnackBar(
+                content: Text('Invalid group code'),
+              ),
+            );
+            break;
+          case JoinGroupErrorType.groupCodeExpired:
+            scaffoldMessengerState.showSnackBar(
+              const SnackBar(
+                content: Text('This group has already ended'),
+              ),
+            );
+            break;
+          case JoinGroupErrorType.groupCodeFull:
+            scaffoldMessengerState.showSnackBar(
+              const SnackBar(
+                content: Text('This group code is full'),
+              ),
+            );
+            break;
+          case JoinGroupErrorType.userAlreadyInGroup:
+            scaffoldMessengerState.showSnackBar(
+              const SnackBar(
+                content: Text('You are already in this group'),
+              ),
+            );
+            break;
+          default:
+        }
+        clean();
+        navigatorState.pop();
+        navigatorState.pop();
+      };
     }
   }
 
-  Future<void> joinGroup(BuildContext context) async {
+  Future<JoinGroupErrorType?> joinGroup(BuildContext context) async {
     final userId = FirebaseAuth.instance.currentUser!.uid;
     final user = Provider.of<AuthProvider>(context, listen: false).user;
     final groups = await FirebaseFirestore.instance
@@ -40,12 +89,25 @@ class JoinGroupProvider extends ChangeNotifier {
         .where('groupCode', isEqualTo: controllerGroupCode.text.toUpperCase())
         .get();
     if (groups.docs.isEmpty) {
-      return;
+      return JoinGroupErrorType.groupCodeInvalid;
+    }
+    final groupDocument = groups.docs.first;
+    final groupData = groupDocument.data();
+    final groupModel = GroupModel.fromMap(groupDocument.id, groupData);
+    if (groupModel.participants.contains(userId)) {
+      return JoinGroupErrorType.userAlreadyInGroup;
+    }
+    if (groupModel.participants.length >=
+        groupModel.maxParticipants) {
+      return JoinGroupErrorType.groupCodeFull;
+    }
+    if (groupModel.endDate!.isBefore(DateTime.now())) {
+      return JoinGroupErrorType.groupCodeExpired;
     }
     if (user == null) {
-      return;
+      return JoinGroupErrorType.groupCodeInvalid;
     }
-    FirebaseFirestore.instance
+    await FirebaseFirestore.instance
         .collection('groups')
         .doc(groups.docs.first.id)
         .update(
@@ -64,6 +126,7 @@ class JoinGroupProvider extends ChangeNotifier {
         )
       },
     );
+    return null;
   }
 
   void updateIndex(int index) {
