c8f1d618c72d6710b8f2f5417f670462f9e9a119
Andre Costa <andre_costa2012@hotmail.com.br>
2023-02-03 09:49:12 -0300

    fix: flexible instagrammable image size (screens)

--- lib/screens/individual_group/components/instagrammable/components/body_instagrammable.dart
+++ lib/screens/individual_group/components/instagrammable/components/body_instagrammable.dart
@@ -4,7 +4,6 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:groupup/core/providers/individual_group_provider.dart';
 import 'package:groupup/core/providers/instagrammable_provider.dart';
 import 'package:groupup/core/widgets/buttons/button.dart';
-import 'package:groupup/design-system.dart';
 import 'package:groupup/models/home_view.dart';
 import 'package:groupup/screens/individual_group/components/instagrammable/components/app_bar_instagrammable.dart';
 import 'package:provider/provider.dart';
@@ -30,130 +29,134 @@ class _BodyInstagrammableState extends State<BodyInstagrammable> {
       controller: instagrammableProvider.screenshotController,
       child: Scaffold(
         appBar: const AppBarInstagrammable(),
-        body: Stack(
-          children: [
-            SvgPicture.asset(
-              'assets/images/instagrammable.svg',
-            ),
-            Positioned(
-              width: Insets.l * 3,
-              bottom: 555,
-              left: 66,
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black, width: 2),
-                  borderRadius: BorderRadius.circular(Insets.l * 3),
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(Insets.l * 3),
-                  child: CachedNetworkImage(
-                    imageUrl: participantData.profilePicture,
-                    fadeInDuration: const Duration(milliseconds: 100),
-                    height: Insets.l * 3,
-                    width: Insets.l * 3,
-                    fit: BoxFit.cover,
-                  ),
-                ),
+        body: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.8,
+          width: MediaQuery.of(context).size.width,
+          child: Stack(
+            children: [
+              SvgPicture.asset(
+                'assets/images/instagrammable.svg',
               ),
-            ),
-            Positioned(
-              width: Insets.l * 2.7,
-              bottom: 546,
-              right: 54,
-              child: ButtonCommonStyle(
-                onPressed: () {
-                  Provider.of<InstagrammableProvider>(context, listen: false)
-                      .choosePicture(context);
-                },
+              Positioned(
+                width: MediaQuery.of(context).size.width * 0.145,
+                bottom: MediaQuery.of(context).size.height * 0.585,
+                left: MediaQuery.of(context).size.width * 0.165,
                 child: Container(
                   decoration: BoxDecoration(
                     border: Border.all(color: Colors.black, width: 2),
-                    borderRadius: BorderRadius.circular(Insets.l * 2.7),
+                    borderRadius: BorderRadius.circular(MediaQuery.of(context).size.width * 0.145),
                   ),
                   child: ClipRRect(
-                    borderRadius: BorderRadius.circular(Insets.l * 2.7),
+                    borderRadius: BorderRadius.circular(MediaQuery.of(context).size.width * 0.145),
                     child: CachedNetworkImage(
                       imageUrl: participantData.profilePicture,
-                      height: Insets.l * 2.7,
-                      width: Insets.l * 2.7,
+                      fadeInDuration: const Duration(milliseconds: 100),
+                      height: MediaQuery.of(context).size.width * 0.145,
+                      width: MediaQuery.of(context).size.width * 0.145,
                       fit: BoxFit.cover,
                     ),
                   ),
                 ),
               ),
-            ),
-            Positioned(
-              width: Insets.l * 3.5,
-              bottom: 260,
-              right: 42,
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black, width: 2),
-                  borderRadius: BorderRadius.circular(Insets.l * 3.5),
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(Insets.l * 3.5),
-                  child: CachedNetworkImage(
-                    imageUrl: participantData.profilePicture,
-                    fadeInDuration: const Duration(milliseconds: 100),
-                    height: Insets.l * 3.5,
-                    width: Insets.l * 3.5,
-                    fit: BoxFit.cover,
+              Positioned(
+                width: MediaQuery.of(context).size.width * 0.127,
+                bottom: MediaQuery.of(context).size.height * 0.575,
+                right: MediaQuery.of(context).size.width * 0.1415,
+                child: ButtonCommonStyle(
+                  onPressed: () {
+                    Provider.of<InstagrammableProvider>(context, listen: false)
+                        .choosePicture(context);
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black, width: 2),
+                      borderRadius: BorderRadius.circular(MediaQuery.of(context).size.width * 0.127),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(MediaQuery.of(context).size.width * 0.127),
+                      child: CachedNetworkImage(
+                        imageUrl: participantData.profilePicture,
+                        height: MediaQuery.of(context).size.width * 0.127,
+                        width: MediaQuery.of(context).size.width * 0.127,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Positioned(
-              width: Insets.l * 3.5,
-              bottom: 295,
-              left: 37,
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black, width: 2),
-                  borderRadius: BorderRadius.circular(Insets.l * 3.5),
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(Insets.l * 3.5),
-                  child: CachedNetworkImage(
-                    imageUrl: participantData.profilePicture,
-                    fadeInDuration: const Duration(milliseconds: 100),
-                    height: Insets.l * 3.5,
-                    width: Insets.l * 3.5,
-                    fit: BoxFit.cover,
+              Positioned(
+                width: MediaQuery.of(context).size.width * 0.165,
+                bottom: MediaQuery.of(context).size.width * 0.58,
+                right: MediaQuery.of(context).size.width * 0.1125,
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 2),
+                    borderRadius: BorderRadius.circular(MediaQuery.of(context).size.width * 0.165),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(MediaQuery.of(context).size.width * 0.165),
+                    child: CachedNetworkImage(
+                      imageUrl: participantData.profilePicture,
+                      fadeInDuration: const Duration(milliseconds: 100),
+                      height: MediaQuery.of(context).size.width * 0.165,
+                      width: MediaQuery.of(context).size.width * 0.165,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Positioned(
-              width: Insets.l * 3.85,
-              bottom: 362,
-              right: 178,
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black, width: 2),
-                  borderRadius: BorderRadius.circular(Insets.l * 3.85),
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(Insets.l * 3.85),
-                  child: CachedNetworkImage(
-                    imageUrl: participantData.profilePicture,
-                    fadeInDuration: const Duration(milliseconds: 100),
-                    height: Insets.l * 3.85,
-                    width: Insets.l * 3.85,
-                    fit: BoxFit.cover,
+              Positioned(
+                width: MediaQuery.of(context).size.width * 0.1575,
+                bottom: MediaQuery.of(context).size.width * 0.6625,
+                left: MediaQuery.of(context).size.width * 0.103,
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 2),
+                    borderRadius: BorderRadius.circular(MediaQuery.of(context).size.width * 0.1575),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(MediaQuery.of(context).size.width * 0.1575),
+                    child: CachedNetworkImage(
+                      imageUrl: participantData.profilePicture,
+                      fadeInDuration: const Duration(milliseconds: 100),
+                      height: MediaQuery.of(context).size.width * 0.1575,
+                      width: MediaQuery.of(context).size.width * 0.1575,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Positioned(
-              width: Insets.l * 3.85,
-              bottom: 425,
-              right: 178,
-              child: SvgPicture.asset(
-                'assets/icons/crown.svg',
+              Positioned(
+                width: MediaQuery.of(context).size.width * 0.18,
+                bottom: MediaQuery.of(context).size.width * 0.82,
+                right: MediaQuery.of(context).size.width * 0.43,
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 2),
+                    borderRadius: BorderRadius.circular(MediaQuery.of(context).size.width * 0.1725),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(MediaQuery.of(context).size.width * 0.1725),
+                    child: CachedNetworkImage(
+                      imageUrl: participantData.profilePicture,
+                      fadeInDuration: const Duration(milliseconds: 100),
+                      height: MediaQuery.of(context).size.width * 0.1725,
+                      width: MediaQuery.of(context).size.width * 0.1725,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
               ),
-            ),
-          ],
+              Positioned(
+                width: MediaQuery.of(context).size.width * 0.17,
+                bottom: MediaQuery.of(context).size.width * 0.96,
+                right: MediaQuery.of(context).size.width * 0.43,
+                child: SvgPicture.asset(
+                  'assets/icons/crown.svg',
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
