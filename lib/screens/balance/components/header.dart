a1d2762d800bc09a6d8250f6cc41fddfda81d508
Andre Costa <andre_costa2012@hotmail.com.br>
2023-02-16 17:11:52 -0300

    feat: options to create, delete, login, withdraw

--- lib/screens/balance/components/header.dart
+++ lib/screens/balance/components/header.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:groupup/constants.dart';
@@ -6,6 +8,9 @@ import 'package:groupup/core/widgets/texts/title.dart';
 import 'package:groupup/design-system.dart';
 import 'package:groupup/screens/balance/components/button.dart';
 import 'package:groupup/core/providers/auth_provider.dart';
+import 'package:groupup/screens/balance/components/continue_create_connected_account_dialog.dart';
+import 'package:groupup/screens/balance/components/create_connected_account_dialog.dart';
+import 'package:groupup/screens/balance/components/payout_or_connected_account_options_dialog.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -36,39 +41,30 @@ class HeaderBalance extends StatelessWidget {
                   fontFamily: 'Montserrat-SemiBold',
                 ),
               ),
-              SizedBox(width: MediaQuery.of(context).size.width * 0.05),
+              SizedBox(width: MediaQuery.of(context).size.width * 0.32),
               Button(
                 onPressed: () async {
-                  try {
-                    await FirebaseFunctions.instance
-                        .httpsCallable('CreateAccount')
-                        .call({});
-                  } on FirebaseFunctionsException catch (e) {
-                    // ignore: avoid_print
-                    print(e.message);
+                  final authProvider =
+                      Provider.of<AuthProvider>(context, listen: false);
+                  if (authProvider.user == null) return;
+                  if (authProvider.user!.stripeAccountId.isEmpty) {
+                    createConnectedAccount(context);
+                  } else {
+                    final getAccount = await FirebaseFunctions.instance
+                        .httpsCallable('GetAccount')
+                        .call({
+                      'accountId': authProvider.user!.stripeAccountId,
+                    });
+                    if (getAccount.data['detailsSubmitted'] == true) {
+                      payoutOrConnectedAccountOptionsDialog(context);
+                    } else {
+                      continueCreateConnectedAccountDialog(context);
+                    }
                   }
                 },
-                text: appLocalizations.add,
+                text: appLocalizations.options,
                 buttonColor: kPrimaryColor,
                 textColor: Colors.white,
-              ),
-              const SizedBox(width: Insets.l),
-              Button(
-                onPressed: () async {
-                  try {
-                    await FirebaseFunctions.instance
-                        .httpsCallable('DeleteAccount')
-                        .call({
-                      'accountId': 'acct_1MaROrIAfr4NdoZf',
-                    });
-                  } on FirebaseFunctionsException catch (e) {
-                    // ignore: avoid_print
-                    print(e.message);
-                  }
-                },
-                text: appLocalizations.withdraw,
-                buttonColor: Color(0XFFE1E1E1),
-                textColor: Colors.black,
               ),
               const SizedBox(height: Insets.l * 2.25),
             ],
