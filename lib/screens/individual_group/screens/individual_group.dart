6679aeba6646b939693708121a89a2321e5b76f6
Andre Costa <andre_costa2012@hotmail.com.br>
2023-02-21 16:08:20 -0300

    feat: when group draws, 3 more days to add data

--- lib/screens/individual_group/screens/individual_group.dart
+++ lib/screens/individual_group/screens/individual_group.dart
@@ -1,8 +1,7 @@
-import 'package:flutter/cupertino.dart';
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:groupup/constants.dart';
-import 'package:groupup/core/widgets/buttons/button.dart';
-import 'package:groupup/core/widgets/texts/static_text.dart';
+import 'package:groupup/core/providers/auth_provider.dart';
 import 'package:groupup/design-system.dart';
 import 'package:groupup/screens/individual_group/components/floating_buttons/add_input_group_button.dart';
 import 'package:groupup/screens/individual_group/components/calendar_add_input/calendar.dart';
@@ -11,13 +10,12 @@ import 'package:groupup/screens/individual_group/components/app_bar.dart';
 import 'package:groupup/screens/individual_group/components/body.dart';
 import 'package:groupup/screens/individual_group/components/floating_buttons/edit_group.dart';
 import 'package:groupup/screens/individual_group/components/floating_buttons/instagrammable_button.dart';
-import 'package:groupup/screens/individual_group/components/group_ended_participant_card.dart';
+import 'package:groupup/screens/individual_group/components/group_ended_dialog.dart';
 import 'package:groupup/screens/individual_group/components/header.dart';
 import 'package:groupup/models/home_view.dart';
 import 'package:groupup/core/providers/individual_group_provider.dart';
-import 'package:intl/intl.dart';
+import 'package:groupup/screens/individual_group/components/tiebreaker_dialog.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class IndividualGroupScreen extends StatefulWidget {
   const IndividualGroupScreen({super.key});
@@ -29,6 +27,7 @@ class IndividualGroupScreen extends StatefulWidget {
 class _IndividualGroupScreenState extends State<IndividualGroupScreen> {
   var willShowEndGroupDialog = false;
   final homeViewModel = HomeViewModel();
+  final keyIsFirstOpened = 'is_first_opened';
 
   @override
   void initState() {
@@ -43,89 +42,48 @@ class _IndividualGroupScreenState extends State<IndividualGroupScreen> {
         Provider.of<IndividualGroupProvider>(context, listen: false).group;
     if (group == null) return;
     if (group.endDate == null) return;
-    if (group.endDate!.isBefore(
-      DateTime.now().subtract(
-        const Duration(
-          days: 1,
+    final participantsSumValue = group.participantsData.map((element) {
+      return element.sumData.value;
+    }).toList();
+    participantsSumValue.sort((a, b) => b.compareTo(a));
+    if (participantsSumValue.length > 1 &&
+        participantsSumValue
+                .where((element) => element == participantsSumValue.first)
+                .length >
+            1) {
+      if (group.endDate!.isBefore(
+        DateTime.now().subtract(
+          const Duration(
+            days: 3,
+          ),
         ),
-      ),
-    )) {
-      if (willShowEndGroupDialog) return;
-      willShowEndGroupDialog = true;
-      WidgetsBinding.instance.addPostFrameCallback(
-        (_) async {
-          if (!mounted) return;
-          return showCupertinoDialog(
-            context: context,
-            builder: (BuildContext context) {
-              final group = Provider.of<IndividualGroupProvider>(context).group;
-              final appLocalizations = AppLocalizations.of(context);
-              String groupCurrencySymbol =
-                  NumberFormat.simpleCurrency(name: group?.groupCurrencyCode)
-                      .currencySymbol;
-              if (group == null) return const SizedBox();
-              return AlertDialog(
-                title: StaticText(
-                  text: appLocalizations.groupEnded,
-                  textAlign: TextAlign.center,
-                  fontFamily: 'Montserrat-SemiBold',
-                  fontSize: TextSize.lBody,
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                content: SizedBox(
-                  width: double.maxFinite,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      StaticText(
-                        text: appLocalizations.winnerWon(groupCurrencySymbol, group.reward),
-                        fontSize: TextSize.mBody,
-                      ),
-                      ListView.separated(
-                        shrinkWrap: true,
-                        separatorBuilder: (context, index) => const Divider(
-                          indent: kDefaultPadding,
-                          endIndent: kDefaultPadding,
-                          height: 20,
-                          thickness: 0.5,
-                          color: kSecondaryColor,
-                        ),
-                        itemCount: group.participantsData.length,
-                        itemBuilder: (context, index) =>
-                            GroupEndedParticipantCard(
-                          participant: group.participantsData[index],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                contentPadding: const EdgeInsets.only(top: 20, bottom: 20),
-                actionsAlignment: MainAxisAlignment.center,
-                actionsPadding: const EdgeInsets.only(bottom: 15),
-                actions: [
-                  SizedBox(
-                    width: double.infinity,
-                    child: ButtonCommonStyle(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                        Navigator.of(context).pop();
-                      },
-                      child: const StaticText(
-                        text: 'OK',
-                        fontSize: TextSize.mBody,
-                        fontFamily: 'Montserrat-SemiBold',
-                        color: kPrimaryColor,
-                      ),
-                    ),
-                  )
-                ],
-              );
-            },
-          );
-        },
-      );
+      )) {
+        if (willShowEndGroupDialog) return;
+        willShowEndGroupDialog = true;
+        WidgetsBinding.instance.addPostFrameCallback(
+          (_) async {
+            if (!mounted) return;
+            return showDialogIfFirstOpened(context);
+          },
+        );
+      }
+    } else {
+      if (group.endDate!.isBefore(
+        DateTime.now().subtract(
+          const Duration(
+            days: 1,
+          ),
+        ),
+      )) {
+        if (willShowEndGroupDialog) return;
+        willShowEndGroupDialog = true;
+        WidgetsBinding.instance.addPostFrameCallback(
+          (_) async {
+            if (!mounted) return;
+            return groupEndedDialog(context);
+          },
+        );
+      }
     }
   }
 
@@ -185,80 +143,157 @@ class _IndividualGroupScreenState extends State<IndividualGroupScreen> {
           ],
         ),
       );
-    }
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBarIndividualGroup(
-        homeViewModel: homeViewModel,
-      ),
-      resizeToAvoidBottomInset: false,
-      body: PageView(
-        controller: individualGroupProvider.pageController,
-        onPageChanged: (value) {
-          individualGroupProvider.updateIndex(value);
-        },
-        physics: const NeverScrollableScrollPhysics(),
-        children: [
-          Column(
-            children: [
-              const HeaderIndividualGroup(),
-              const SizedBox(height: kDefaultPadding),
-              BodyIndividualGroup(
-                homeViewModel: homeViewModel,
-              )
-            ],
-          ),
-          Column(
-            children: const [
-              Calendar(),
-            ],
-          ),
-        ],
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.symmetric(
-          vertical: Insets.m,
-          horizontal: Insets.l,
+    } else {
+      final participantsSumValue =
+          individualGroupProvider.group!.participantsData.map((element) {
+        return element.sumData.value;
+      }).toList();
+      participantsSumValue.sort((a, b) => b.compareTo(a));
+
+      final dateTimeNow = DateTime.now();
+      final dateTimeYesterday = dateTimeNow.subtract(
+        const Duration(
+          days: 1,
         ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.end,
+      );
+      final dateTimeThreeDaysAgo = dateTimeNow.subtract(
+        const Duration(
+          days: 3,
+        ),
+      );
+      final currentUser = Provider.of<AuthProvider>(context).user;
+      if (currentUser == null) return Container();
+      final currentUserSumValue = individualGroupProvider
+          .group!.participantsData
+          .firstWhere((element) => element.uid == currentUser.id)
+          .sumData
+          .value;
+      return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBarIndividualGroup(
+          homeViewModel: homeViewModel,
+        ),
+        resizeToAvoidBottomInset: false,
+        body: PageView(
+          controller: individualGroupProvider.pageController,
+          onPageChanged: (value) {
+            individualGroupProvider.updateIndex(value);
+          },
+          physics: const NeverScrollableScrollPhysics(),
           children: [
-            const InstagrammableButton(),
             Column(
-              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                EditAndHistoryGroupButton(
+                const HeaderIndividualGroup(),
+                const SizedBox(height: kDefaultPadding),
+                BodyIndividualGroup(
                   homeViewModel: homeViewModel,
-                ),
-                if (individualGroupProvider.group == null) const SizedBox(),
-                if (individualGroupProvider.group?.endDate == null)
-                  const SizedBox(),
-                individualGroupProvider.group!.endDate!
-                            .isBefore(DateTime.now()) &&
-                        individualGroupProvider.group!.endDate!.isAfter(
-                            DateTime.now().subtract(const Duration(days: 1)))
-                    ? const SizedBox()
-                    : const SizedBox(height: kDefaultPadding),
-                if (individualGroupProvider.group == null) const SizedBox(),
-                if (individualGroupProvider.group?.endDate == null)
-                  const SizedBox(),
-                individualGroupProvider.group!.endDate!
-                            .isBefore(DateTime.now()) &&
-                        individualGroupProvider.group!.endDate!.isAfter(
-                            DateTime.now().subtract(const Duration(days: 1)))
-                    ? const SizedBox()
-                    : individualGroupProvider.pageIndex == 0
-                        ? CalendarScreenButton(
-                            homeViewModel: homeViewModel,
-                          )
-                        : AddInputGroupButton(homeViewModel: homeViewModel),
+                )
+              ],
+            ),
+            Column(
+              children: const [
+                Calendar(),
               ],
             ),
           ],
         ),
-      ),
-    );
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: Insets.m,
+            horizontal: Insets.l,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              const InstagrammableButton(),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  EditAndHistoryGroupButton(
+                    homeViewModel: homeViewModel,
+                  ),
+                  if (individualGroupProvider.group == null) const SizedBox(),
+                  if (individualGroupProvider.group?.endDate == null)
+                    const SizedBox(),
+                  if (participantsSumValue.length > 1 &&
+                      participantsSumValue
+                              .where((element) =>
+                                  element == participantsSumValue.first)
+                              .length >
+                          1)
+                    if (individualGroupProvider.group!.endDate!
+                            .isBefore(dateTimeNow) &&
+                        individualGroupProvider.group!.endDate!.isAfter(
+                          dateTimeThreeDaysAgo,
+                        ))
+                      if (participantsSumValue.first == currentUserSumValue)
+                        const SizedBox(height: kDefaultPadding)
+                      else
+                        const SizedBox()
+                    else
+                      const SizedBox()
+                  else if (individualGroupProvider.group!.endDate!
+                          .isBefore(dateTimeNow) &&
+                      individualGroupProvider.group!.endDate!.isAfter(
+                        dateTimeYesterday,
+                      ))
+                    const SizedBox(height: kDefaultPadding)
+                  else if (individualGroupProvider.group!.endDate!
+                      .isBefore(dateTimeNow))
+                    const SizedBox()
+                  else
+                    const SizedBox(height: kDefaultPadding),
+                  if (individualGroupProvider.group == null) const SizedBox(),
+                  if (individualGroupProvider.group?.endDate == null)
+                    const SizedBox(),
+                  if (participantsSumValue.length > 1 &&
+                      participantsSumValue
+                              .where((element) =>
+                                  element == participantsSumValue.first)
+                              .length >
+                          1)
+                    if (individualGroupProvider.group!.endDate!
+                            .isBefore(dateTimeNow) &&
+                        individualGroupProvider.group!.endDate!.isAfter(
+                          dateTimeThreeDaysAgo,
+                        ))
+                      if (participantsSumValue.first == currentUserSumValue)
+                        individualGroupProvider.pageIndex == 0
+                            ? CalendarScreenButton(
+                                homeViewModel: homeViewModel,
+                              )
+                            : AddInputGroupButton(homeViewModel: homeViewModel)
+                      else
+                        const SizedBox()
+                    else
+                      const SizedBox()
+                  else if (individualGroupProvider.group!.endDate!
+                          .isBefore(dateTimeNow) &&
+                      individualGroupProvider.group!.endDate!.isAfter(
+                        dateTimeYesterday,
+                      ))
+                    individualGroupProvider.pageIndex == 0
+                        ? CalendarScreenButton(
+                            homeViewModel: homeViewModel,
+                          )
+                        : AddInputGroupButton(homeViewModel: homeViewModel)
+                  else if (individualGroupProvider.group!.endDate!
+                      .isBefore(dateTimeNow))
+                    const SizedBox()
+                  else
+                    individualGroupProvider.pageIndex == 0
+                        ? CalendarScreenButton(
+                            homeViewModel: homeViewModel,
+                          )
+                        : AddInputGroupButton(homeViewModel: homeViewModel)
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    }
   }
 }
