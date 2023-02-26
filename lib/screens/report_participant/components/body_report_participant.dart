b4c9781114ef2d0e76ec29bf344968e5a128a792
Andre Costa <andre_costa2012@hotmail.com.br>
2023-02-25 21:29:06 -0300

    feat: dont show current user on report part.

--- lib/screens/report_participant/components/body_report_participant.dart
+++ lib/screens/report_participant/components/body_report_participant.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:groupup/constants.dart';
+import 'package:groupup/core/providers/auth_provider.dart';
 import 'package:groupup/core/providers/individual_group_provider.dart';
 import 'package:groupup/screens/report_participant/components/individual_report_participant.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import '../../../core/widgets/texts/static_text.dart';
 
 class BodyReportParticipant extends StatelessWidget {
   const BodyReportParticipant({super.key});
@@ -13,12 +17,40 @@ class BodyReportParticipant extends StatelessWidget {
       child: Scrollbar(
         child: Builder(
           builder: (context) {
+            final currentUser = Provider.of<AuthProvider>(context).user;
+            if (currentUser == null) {
+              return const Center(
+                child: CircularProgressIndicator(color: kPrimaryColor),
+              );
+            }
             final participants = Provider.of<IndividualGroupProvider>(context)
                 .group
-                ?.participantsData;
+                ?.participantsData
+                .where((element) => element.uid != currentUser.id)
+                .toList();
             if (participants == null) {
               return const Center(
                 child: CircularProgressIndicator(color: kPrimaryColor),
+              );
+            }
+            if (participants.isEmpty) {
+              return Column(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: kDefaultPadding),
+                      child: Align(
+                        alignment: Alignment.topCenter,
+                        child: StaticText(
+                          text: AppLocalizations.of(context).noParticipants,
+                          color: kSecondaryColor,
+                          textAlign: TextAlign.center,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               );
             }
             return ListView.separated(
