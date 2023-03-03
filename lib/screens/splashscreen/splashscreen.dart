b9c90990825cd782242df2305488080edcfd47db
Andre Costa <andre_costa2012@hotmail.com.br>
2023-03-03 19:24:46 -0300

    feat: splashscreen commented

--- lib/screens/splashscreen/splashscreen.dart
+++ lib/screens/splashscreen/splashscreen.dart
@@ -1,46 +1,46 @@
-import 'dart:async';
-import 'package:flutter/material.dart';
-import 'package:groupup/screens/home/screens/home.dart';
+// import 'dart:async';
+// import 'package:flutter/material.dart';
+// import 'package:groupup/screens/home/screens/home.dart';
 
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+// class SplashScreen extends StatefulWidget {
+//   const SplashScreen({super.key});
 
-  @override
-  State<StatefulWidget> createState() {
-    return StartState();
-  }
-}
+//   @override
+//   State<StatefulWidget> createState() {
+//     return StartState();
+//   }
+// }
 
-class StartState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    startTimer();
-  }
+// class StartState extends State<SplashScreen> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     startTimer();
+//   }
 
-  startTimer() async {
-    var duration = const Duration(seconds: 0);
-    return Timer(duration, route);
-  }
+//   startTimer() async {
+//     var duration = const Duration(seconds: 0);
+//     return Timer(duration, route);
+//   }
 
-  route() {
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(
-        builder: (context) => Home(),
-        settings: const RouteSettings(name: 'Home'),
-      ),
-    );
-  }
+//   route() {
+//     Navigator.pushReplacement(
+//       context,
+//       MaterialPageRoute(
+//         builder: (context) => Home(),
+//         settings: const RouteSettings(name: 'Home'),
+//       ),
+//     );
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Image.asset(
-        'assets/images/SplashScreen.png',
-        height: double.infinity,
-        width: double.infinity,
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Image.asset(
+//         'assets/images/SplashScreen.png',
+//         height: double.infinity,
+//         width: double.infinity,
+//       ),
+//     );
+//   }
+// }
