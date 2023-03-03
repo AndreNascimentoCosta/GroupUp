9aa46a2d400a8068627e16044a1f3b23edbfa87d
Andre Costa <andre_costa2012@hotmail.com.br>
2023-03-03 19:39:11 -0300

    fix: splashscreen commented

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:groupup/screens/splashscreen/splashscreen.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const SplashScreen());
+    // await tester.pumpWidget(const SplashScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
