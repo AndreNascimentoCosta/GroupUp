65a12eddfb9cb140089265eb2f71d10d3ef712f3
Andre Costa <andre_costa2012@hotmail.com.br>
2023-03-07 17:05:23 -0300

    fix: header overflow in android

--- lib/screens/individual_group/components/header.dart
+++ lib/screens/individual_group/components/header.dart
@@ -169,7 +169,7 @@ class HeaderIndividualGroup extends StatelessWidget {
                       ? screenWidth * 0.05
                       : isSmallScreen
                           ? screenWidth * 0.075
-                          : screenWidth * 0.13,
+                          : screenWidth * 0.125,
                   thickness: 1,
                   color: kSecondaryColor,
                 ),
@@ -237,7 +237,7 @@ class HeaderIndividualGroup extends StatelessWidget {
                       ? screenWidth * 0.06
                       : isSmallScreen
                           ? screenWidth * 0.075
-                          : screenWidth * 0.13,
+                          : screenWidth * 0.125,
                   thickness: 1,
                   color: kSecondaryColor,
                 ),
