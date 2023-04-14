25994c02fa27ac7c335d6aa4c7def06b38947f8c
Andre Costa <andre_costa2012@hotmail.com.br>
2023-04-14 11:42:17 -0300

    feat: rive animations as constants

---
+++ lib/animations/rive_animations.dart
@@ -0,0 +1,6 @@
+class RiveAnimations {
+  static const String addDataAndPhoto = 'assets/animations/add_data_and_photo_onboarding.riv';
+  static const String createGroup = 'assets/animations/create_group_onboarding.riv';
+  static const String validateData = 'assets/animations/validate_data_onboarding.riv';
+  static const String winReward = 'assets/animations/win_reward_onboarding.riv';
+}
