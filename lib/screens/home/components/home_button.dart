5886b7268501bd7d9f628bd90f97304e25123264
Andre Costa <andre_costa2012@hotmail.com.br>
2023-04-14 18:27:13 -0300

    refactor: svg to icon wrapper

--- lib/screens/home/components/home_button.dart
+++ lib/screens/home/components/home_button.dart
@@ -4,11 +4,13 @@ import 'package:groupup/constants.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:groupup/core/extensions/gp_size_extension.dart';
 import 'package:groupup/core/utils/colors/gp_colors.dart';
+import 'package:groupup/core/utils/icons/gp_icons.dart';
+import 'package:groupup/core/widgets/icons/gp_icon.dart';
 
 class HomeButton extends StatelessWidget {
-  const HomeButton({required this.isGreen});
+  const HomeButton({required this.isJoinButton});
 
-  final bool isGreen;
+  final bool isJoinButton;
 
   @override
   Widget build(BuildContext context) {
@@ -17,32 +19,36 @@ class HomeButton extends StatelessWidget {
     final isSmallScreen = screenHeight < 800 || screenWidth < 350;
     return Container(
       padding: EdgeInsets.only(
-        right: isGreen ? kDefaultPadding * 1.25 : kDefaultPadding * 0.75,
+        right: isJoinButton ? kDefaultPadding * 1.25 : kDefaultPadding * 0.75,
         left: kDefaultPadding,
       ),
       height: isSmallScreen ? screenHeight * 0.125 : screenHeight * 0.105,
       decoration: BoxDecoration(
-        color: isGreen ? const Color(0xFFEBEBEB) : const Color(0xFF46E297),
+        color: isJoinButton
+            ? const Color(0xFFEBEBEB)
+            : const Color(
+                0xFF46E297,
+              ),
         borderRadius: BorderRadius.circular(20),
       ),
       alignment: Alignment.centerLeft,
       child: Row(
         children: [
           Text(
-            isGreen ? AppLocalizations.of(context).joinAGroup : AppLocalizations.of(context).createNewGroup,
+            isJoinButton
+                ? AppLocalizations.of(context).joinAGroup
+                : AppLocalizations.of(context).createNewGroup,
             style: TextStyle(
               fontFamily: 'Montserrat-SemiBold',
               fontSize: 20,
-              color: isGreen ? GPColors.black : GPColors.white,
+              color: isJoinButton ? GPColors.black : GPColors.white,
             ),
           ),
           const Spacer(),
-          SvgPicture.asset(
-            isGreen
-                ? 'assets/icons/arrow_right.svg'
-                : 'assets/icons/plus.svg',
-            height: isGreen ? 26.0 : 32.67,
-            width: isGreen ? 15.0 : 32.67,
+          GPIcon(
+            isJoinButton ? GPIcons.arrowRight : GPIcons.plus,
+            height: isJoinButton ? 26.0 : 32.67,
+            width: isJoinButton ? 15.0 : 32.67,
           ),
         ],
       ),
