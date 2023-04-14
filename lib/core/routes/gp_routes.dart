139e24b5fecd53c9d970c87a3b5eb73958c11864
Andre Costa <andre_costa2012@hotmail.com.br>
2023-04-14 11:41:34 -0300

    feat: routes and named routes created

---
+++ lib/core/routes/gp_routes.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:groupup/core/routes/gp_named_routes.dart';
+import 'package:groupup/screens/home/pages/home.dart';
+
+class GPRoute {
+  const GPRoute._();
+
+  factory GPRoute() => const GPRoute._();
+
+  static Route<dynamic>? generateRoute(RouteSettings settings) {
+    /// Getting the route name that were passed to the route.
+    final routeName = settings.name;
+
+    switch (routeName) {
+      case GPNamedRoutes.home:
+        return MaterialPageRoute(
+          builder: (context) => const Home(),
+        );
+    }
+    return null;
+  }
+}
