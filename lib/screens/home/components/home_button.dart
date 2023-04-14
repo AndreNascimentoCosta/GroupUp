ecee31380e5eff109b0766c4fbedd4957c66aa99
Andre Costa <andre_costa2012@hotmail.com.br>
2023-04-14 18:28:55 -0300

    style: unused import removed

--- lib/screens/home/components/home_button.dart
+++ lib/screens/home/components/home_button.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:groupup/constants.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:groupup/core/extensions/gp_size_extension.dart';
