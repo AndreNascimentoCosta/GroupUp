587151ddd8a05e12cd8e18401e91efddcaacfeb1
Andre Costa <andre_costa2012@hotmail.com.br>
2023-04-23 09:21:37 -0300

    style: unused import removed

--- lib/screens/individual_group_settings/edit_fields/edit_dates/components/date_time_picker_edit_group_dates.dart
+++ lib/screens/individual_group_settings/edit_fields/edit_dates/components/date_time_picker_edit_group_dates.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:groupup/core/constants/constants.dart';
 import 'package:groupup/core/extensions/gp_size_extension.dart';
 import 'package:groupup/core/utils/colors/gp_colors.dart';
