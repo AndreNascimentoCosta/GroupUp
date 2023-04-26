218b85b75985c913cee6cf245e6504b7c7c81d35
Andre Costa <andre_costa2012@hotmail.com.br>
2023-04-26 10:30:53 -0300

    style: changed file place

--- lib/screens/onboarding/pages/win_reward_onboarding.dart
+++ lib/screens/onboarding/pages/win_reward_onboarding.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:groupup/animations/rive_animations.dart';
+import 'package:groupup/core/animations/rive_animations.dart';
 import 'package:groupup/core/constants/constants.dart';
 import 'package:groupup/screens/onboarding/components/standard_body_onboarding.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
