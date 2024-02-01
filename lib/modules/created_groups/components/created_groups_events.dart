3645d473028c341e4170acd56499a1b69ca9ad1b
Andre Costa <andre_costa2012@hotmail.com.br>
2024-02-01 19:24:09 -0300

    feat: created group app bar edited

---
+++ lib/modules/created_groups/components/created_groups_events.dart
@@ -0,0 +1,8 @@
+enum CreatedGroupsEvents {
+
+  pressBackButtonCreatedGroups('press_back_button_created_groups');
+
+  final String value;
+
+  const CreatedGroupsEvents(this.value);
+}
--- lib/modules/created_groups/screens/created_groups_screen.dart
+++ lib/modules/created_groups/screens/created_groups_screen.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:groupup/core/utils/colors/gp_colors.dart';
-import 'package:groupup/modules/created_groups/components/created_groups_app_bar.dart';
+import 'package:groupup/core/widgets/app_bar/gp_app_bar.dart';
 import 'package:groupup/modules/created_groups/components/created_groups_body.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:groupup/modules/created_groups/components/created_groups_events.dart';
+import 'package:groupup/modules/edit_profile/screens/edit_profile.dart';
 
 class CreatedGroupsScreen extends StatelessWidget {
   const CreatedGroupsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final appLocalizations = AppLocalizations.of(context)!;
+    return Scaffold(
       backgroundColor: GPColors.white,
-      appBar: AppBarCreatedGroups(),
-      body: BodyCreatedGroup(),
+      appBar: GPAppBar(
+        headerText: appLocalizations.createdGroups,
+        eventName: CreatedGroupsEvents.pressBackButtonCreatedGroups.value,
+        route: const EditProfileScreen(),
+      ),
+      body: const BodyCreatedGroup(),
     );
   }
 }
